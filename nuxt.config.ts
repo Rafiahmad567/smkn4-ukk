a5e138389f04966da8f3542fe57f617116010534
Rafiahmad567 <142226654+Rafiahmad567@users.noreply.github.com>
2025-01-04 14:30:52 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  css: ["Bootstrap/dist/css/Bootstrap.min.css"],
+  css: ["bootstrap/dist/css/bootstrap.min.css"],
 })
